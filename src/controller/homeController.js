279fe6b6ae6dffad5a43314b38da96893bf4b41c
Duong Dev <duongsulam1234@gmail.com>
2023-06-23 17:22:54 +0700

    update someshjt

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -2,10 +2,12 @@ import connection from "../config/connectDB";
 
 let getHomepage = (req, res) => {
   let data = [];
+
+  // Query database bỏ vào biến data 
   connection.query("SELECT * FROM `users`", 
   function (err, results, fields) {
     console.log(results); // results contains rows returned by server
-    data = results.map((row) => { return row});
+    data = results.map((rows) => { return rows});
     return res.render("test/index.ejs", {dataUser: JSON.stringify(data)});
   });
 
