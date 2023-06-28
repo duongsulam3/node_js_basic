9a5128de71cbd5070c744456a49f153298325abd
Duong Dev <duongsulam1234@gmail.com>
2023-06-28 17:20:14 +0700

    bai 11 - build Detail User page

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -20,7 +20,17 @@ let getAboutMe = (req, res) => {
   return res.send("I'm Dương");
 };
 
+let getDetailUser = async (req, res) => {
+  let userID = req.params.id;
+  let [users] = await (
+    await connection
+  ).execute("SELECT * FROM `users` WHERE id = ?", [userID]);
+  console.log(users);
+  return res.send(JSON.stringify(users[0]));
+};
+
 export default {
   getHomepage,
   getAboutMe,
+  getDetailUser,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,16 +1,17 @@
 import express from "express";
 import homeController from "../controller/homeController";
 
-
 let router = express.Router();
 
 const initWebRoute = (app) => {
-    //Trang Home
-    router.get('/', homeController.getHomepage)
-    //Trang About Me
-    router.get('/about', homeController.getAboutMe)
-    
-    return app.use('/api/test', router);
-}
+  //Trang Home
+  router.get("/", homeController.getHomepage);
+  //Trang About Me
+  router.get("/about", homeController.getAboutMe);
+  //Detail User
+  router.get("/detail/user/:id", homeController.getDetailUser)
+
+  return app.use("/api/test", router);
+};
 
 export default initWebRoute;
