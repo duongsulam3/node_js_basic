e55f9d5f6cead217c177ca9c31bd8039f08d2dc0
Duong Dev <duongsulam1234@gmail.com>
2023-06-29 23:47:40 +0700

    updated

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -2,15 +2,6 @@ import connection from "../config/connectDB";
 
 let getHomepage = async (req, res) => {
   //Query database
-  //let data = [];
-  // connection.query("SELECT * FROM `users`", function (err, results, fields) {
-  //   console.log(results); // results contains rows returned by server
-  //   data = results.map((rows) => {
-  //     return rows;
-  //   });
-  //   return res.render("test/index.ejs", { dataUser: data });
-  // });
-
   const [rows] = await (await connection).execute("SELECT * FROM `users`");
   console.log("Connected database");
   return res.render("test/index.ejs", { dataUser: rows });
